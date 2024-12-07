dd9d7e75b3fed97caf27670d190245f9d20eaf30
Yusuff Sodiq <ysodiqakanni@gmail.com>
2024-12-06 23:24:55 -0600

    Add project files.

---
+++ Swizzle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Swizzle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
