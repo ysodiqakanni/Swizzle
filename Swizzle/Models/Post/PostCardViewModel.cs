8ff929168b9bbe1a23eb4bfad3a6942ac80e91c0
Yusuff Sodiq <ysodiqakanni@gmail.com>
2024-12-19 12:17:12 -0600

    dynamic post loading on homepage.

---
+++ Swizzle/Models/Post/PostCardViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Swizzle.Models.Post
+{
+    public class PostCardViewModel
+    {
+        public string PostId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string VoteCount { get; set; }
+        public string Community { get; set; }
+        public string PosterName { get; set; }
+        public string TimePosted { get; set; }
+        public string CommentCount { get; set; }
+        public bool HasMedia { get; set; }
+        public string MediaType { get; set; } = "image";
+        public string MediaUrl { get; set; }
+    }
+}
