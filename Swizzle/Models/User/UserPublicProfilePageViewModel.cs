4aec1121b3ca67bf7ac03f6836cad9f60a8b641f
Yusuff Sodiq <ysodiqakanni@gmail.com>
2024-12-21 13:05:42 -0600

    create user controller

---
+++ Swizzle/Models/User/UserPublicProfilePageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Swizzle.Models.Post;
+
+namespace Swizzle.Models.User
+{
+    public class UserPublicProfilePageViewModel
+    {
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string About { get; set; }
+        public string CoverPhotoUrl { get; set; }
+        public string LogoUrl { get; set; }
+        public string PostsCount { get; set; }
+        public string CommentsCount { get; set; }
+        public string JoinedDate { get; set; }
+
+
+        public List<PostCardViewModel>? PagePosts { get; set; } = new List<PostCardViewModel>();
+    }
+}
